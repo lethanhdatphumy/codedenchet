635f4abc7e2eff5607140f357b6f0f95b259a1fc
lethanhdatphumy <ltd.bqlda7112004@gmail.com>
2023-06-07 09:55:03 +0700

    selection sort

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,37 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-int main (){
-    cout<<"hello world"<<endl;
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap the minimum element with the current element
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    selectionSort(arr, size);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
-} 
+}s
