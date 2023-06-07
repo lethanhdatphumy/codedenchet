a3a8e28a33fa74922ec75f56e0705569ef220630
lethanhdatphumy <ltd.bqlda7112004@gmail.com>
2023-06-07 09:41:46 +0700

    lab 1

--- code.java
+++ code.java
@@ -1,34 +1,64 @@
-import java.time.temporal.TemporalUnit;
 import java.util.Scanner;
-public class code{
-    static void sum( int first_num, int second_num){
-        System.out.println(first_num+second_num);
-        
+
+public class code {
+    static void sum(int first_num, int second_num) {
+        System.out.println("this is result: " + (first_num + second_num));
+
     }
-    static void array(){
-        int [] my_array= {1,2,3,4,5,6,7,8};
-        int len = my_array.length;
-        for(int i =0; i<len; i++){
-            System.out.println(my_array[i]);
+
+    static void array() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Length of your array: ");
+        int len = sc.nextInt();
+        int[] my_array = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            System.out.println("Element " + (i + 1));
+            my_array[i] = sc.nextInt();
         }
-        System.out.print("This is bubble sort: ");
-        int temp; 
-        for (int i =0; i<len; i++){
-            for (int j =i+1; j<len;j++){
-                if (my_array[i]>my_array[j]){
-                    temp=my_array[i];
-                    my_array[i]=my_array[j];
-                    my_array[j]=temp;
+        bubble_sort(my_array, len);
+        selection_sort(my_array, len);
+    }
+
+    static void bubble_sort(int[] my_array, int len) {
+        int temp;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (my_array[i] > my_array[j]) {
+                    temp = my_array[i];
+                    my_array[i] = my_array[j]; 
+                    my_array[j] = temp;
                 }
             }
         }
-        System.out.println(my_array[len-1] );
+        System.out.println("this is the result after applying bubble sort: " + my_array[len - 1]);
+
     }
-    public static void main(String []args){
-        Scanner sc = new Scanner (System.in);
+    static void selection_sort(int [] my_array, int len){
+        int min_index;
+        for (int i =0;i<len;i++){
+            min_index=i;
+            for(int j =i+1;j<len;j++){
+                if (my_array[j]<my_array[min_index]){
+                    min_index = j;
+                }
+
+            }
+            int temp = my_array[i];
+            my_array[i]=my_array[min_index];
+            my_array[min_index]=temp;
+            
+        }
+        System.out.println("this is the result after applying selection sort: " + my_array[len - 1]);
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int first_num = sc.nextInt();
         int second_num = sc.nextInt();
-        sum(first_num,second_num);
+        sum(first_num, second_num);
         array();
 
     }
