4fc9291eb832c0d4ed9a38d6510e63cc77382a87
Đạtdy Dảk <90812330+lethanhdatphumy@users.noreply.github.com>
2023-06-20 20:09:41 +0700

    lab2

--- paithan.py
+++ paithan.py
@@ -1,2 +1,44 @@
-print("hello, world")
+import sys
+import random 
+import time
+from Adafruit_IO import MQTTClient
 
+AIO_FEED_ID = ["sensor1", "sensor2", "sensor3"]
+AIO_USERNAME = "daddyDat"
+AIO_KEY = ""
+
+
+def connected(client):
+    print("Connected...")
+    for i in range(len(AIO_FEED_ID)):
+        client.subscribe(AIO_FEED_ID[i])
+
+
+def subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed...")
+
+
+def disconnected(client):
+    print("Disconnected...")
+    sys.exit(1)
+
+
+def message(client, feed_id, payload):
+    print("recieved data: "+payload)
+client=MQTTClient(AIO_USERNAME,AIO_KEY)
+client.on_connect=connected
+client.on_disconnect=disconnected
+client.on_message=message
+client.on_subscribe=subscribe
+client.connect()
+client.loop_background()
+while True:
+    time.sleep(5)
+    value1 = random.randint(0, 50)
+    value2 = random.randint(0, 100)
+    value3 = random.randint(0, 100)
+    print("Updating ...")
+    client.publish("sensor1", value1)
+    client.publish("sensor2", value2)
+    client.publish("sensor3", value3)
+    pass
